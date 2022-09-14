5f1808dd8dede8d672968cecce20f5f8c2cb4326
Gilmar Junior <gilmar.stack@gmail.com>
2022-09-14 01:23:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// Components / Property
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+      <Button title="Hello World"/>
+    </div>
+  )
+}
+
+export default App
